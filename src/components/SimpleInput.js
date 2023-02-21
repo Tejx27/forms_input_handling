6cc54e74ad88a022148616b366d54ede5a9c5333
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-02-21 10:43:50 +0530

    to clear input fields

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -15,13 +15,15 @@ const SimpleInput = (props) => {
     console.log(enterdName);
     const enteredValue = nameInputRef.current.value; 
     console.log(enteredValue);
+
+    setEnteredName('');
   };
 
   return (
     <form onSubmit={formSubmissionHandler}> 
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} value={enterdName}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>

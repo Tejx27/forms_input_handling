2564398cbf63ef6a5905c6b106e0dde7be0ab22f
Tejas Nikam <Tejasnikam4515@gmail.com>
2023-02-21 10:39:57 +0530

    used useSatet and useRef

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,27 @@
+import React, {useRef, useState} from 'react';
+
 const SimpleInput = (props) => {
+  const nameInputRef=useRef();
+
+  const [enterdName, setEnteredName] = useState('');
+
+  const nameInputChangeHandler = (event) =>{
+    setEnteredName(event.target.value); 
+  }; 
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    console.log(enterdName);
+    const enteredValue = nameInputRef.current.value; 
+    console.log(enteredValue);
+  };
+
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}> 
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler} />
       </div>
       <div className="form-actions">
         <button>Submit</button>
